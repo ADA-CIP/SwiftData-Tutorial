2068bea4bea8d3a51362b2a3aaa758fcb53f2f8c
ramaadi <17980909+rama-adi@users.noreply.github.com>
2024-05-10 11:37:54 +0700

    Initial Commit

---
+++ TodoSwiftUI/TodoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoSwiftUIApp.swift
+//  TodoSwiftUI
+//
+//  Created by Rama Adi Nugraha on 10/05/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
